d0e25f0316704669e2d846ebc16437b2876c0ac9
Egoricetea <144737395+Egoricetea@users.noreply.github.com>
2023-10-27 12:59:10 +0300

    Add files via upload

---
+++ dkr_1/Задание 1.pas
@@ -0,0 +1,23 @@
+﻿var
+  c, d, x: real;
+
+begin
+  writeln('Введите X');
+  readln(x);
+  if x < -7 then
+  begin
+    c :=ln(x)/ln(10) - (x*x*x);
+writeln(c);
+end;  
+  if x < -7 then
+      if x > -5 then
+      begin
+        c :=exp(1/3*ln(x))/exp(0.1*(x)*ln(x))
+end;
+  writeln('Такой функции не существует');
+  if x >= -5 then
+       begin
+    c := (ln(x)/(-x))*(sin(x)/(x*x));
+writeln(c);
+end;
+end.
---
+++ dkr_1/Задание 2.pas
@@ -0,0 +1,24 @@
+﻿var
+  c, d, x: real;
+begin
+  x:=-9;
+ while x<7 do begin
+  if x < -7 then
+  begin
+    c := ln(x)/ln(10) - (x*x*x);
+writeln(c);
+end;  
+  if x < -7 then
+      if x > -5 then
+      begin
+        c :=exp(1/3*ln(x))/exp(0.1*(x)*ln(x))
+end;
+  writeln('Такой функции не существует');
+  if x >= -5 then
+       begin
+    c := (ln(x)/(-x))*(sin(x)/(x*x));
+writeln(c);
+    end;
+  x:=x+0.3;
+  end;
+end.
